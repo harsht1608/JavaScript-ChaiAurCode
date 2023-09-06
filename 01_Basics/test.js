795bae143a37cb213ee94297343c81a63de5c1fb
harsht1608 <91127719+harsht1608@users.noreply.github.com>
2023-09-06 11:53:26 +0000

    type conversion

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,14 @@
-console.log("HelloWorld!")
+// console.log("HelloWorld!")
+const accountId = 123
+let accountEmail = "harsh@gmail.com"
+var accountPassword = "123654"
+accountCity = "Jaipur"
+
+accountEmail = "as@as.com"
+accountPassword = "21212112212"
+accountCity = "Bengaluru"
+
+
+
+
+console.log([accountEmail,accountId,accountPassword,accountCity]);
